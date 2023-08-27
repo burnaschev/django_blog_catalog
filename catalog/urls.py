84def59bbb9d63ed9c9e2379622ae389ae7d8464
Артур <informal201144@gmai;.com>
2023-08-27 13:43:10 +0500

    added 1 lesson

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from config.urls import path
-from catalog.views import product, category, index, contacts
+from catalog.views import index, contacts, CategoryListView, ProductListView
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
@@ -7,8 +7,8 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', index, name='home'),
-    path('category/', category, name='category'),
-    path('<int:pk>/pruduct/', product, name='product'),
+    path('category/', CategoryListView.as_view(), name='category'),
+    path('<int:pk>/pruduct/', ProductListView.as_view(), name='product'),
     path('contacts/', contacts, name='contacts'),
 
 ]
